d4af2fb3c9b957f8ef4b4b9a95b7061b3ff8a51c
rafadantas06 <rafadantas.0616@gmail.com>
2025-08-12 22:09:57 -0300

    Create ativ1.c

---
+++ ativ1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main() {
+	// imprime algumas mensagens na tela
+	printf("Mackenzie - FCI\n");
+	printf("Algoritmos e ");
+	printf("Prog. II\n");
+	printf("Turma 02P\n");
+	
+	// pausa antes de terminar o programa
+	system("pause");
+	return 0;
+}
